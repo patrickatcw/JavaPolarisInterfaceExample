d05097abbe63986e79baf952307dea0611e69243
patrickatcw <pj8392@yahoo.com>
2018-04-28 12:10:51 -0400

    some changes in main, same result

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -7,16 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
         Dog dog = new Dog("Jax", "bark");
-        dog.name();
-        dog.soundAnimalMakes();
+        /*dog.name();
+        dog.soundAnimalMakes();*/
         System.out.println(dog.name());
         System.out.println(dog.soundAnimalMakes());
 
         System.out.println("========================");
 
         Horse horse = new Horse("Mr. Ed", "Hey Wilbur");
-        horse.name();
-        horse.soundAnimalMakes();
+        /*horse.name();
+        horse.soundAnimalMakes();*/
         System.out.println(horse.name());
         System.out.println(horse.soundAnimalMakes());
 
